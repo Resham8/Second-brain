0aa6657d157121ab4cf69b71322c8895515235c4
Resham <reshamupadhyay48@gmail.com>
2025-05-27 22:06:20 +0530

    added api endpoints and middleware

---
+++ Backend/src/index.ts
@@ -0,0 +1,136 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+dotenv.config();
+
+import { z } from "zod";
+import { ContentModel, UserModel } from "./db";
+import { userMiddleware } from "./middleware";
+
+const uri = process.env.MONGOOSE_URI;
+const saltRounds = parseInt(process.env.SALT_ROUNDS || "4");
+
+if (!uri) {
+  throw new Error("MONGO_URI is missing in environment variables");
+}
+
+// console.log("MONGO_URI:", process.env.MONGOOSE_URI);
+
+mongoose.connect(uri);
+
+const app = express();
+app.use(express.json());
+
+const userSchema = z.object({
+  username: z.string().min(3).max(10),
+  password: z
+    .string()
+    .min(8)
+    .max(20)
+    .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/),
+});
+
+app.post("/api/v1/signup", async (req, res) => {
+  const validatedData = userSchema.safeParse(req.body);
+
+  const { username, password } = req.body;
+  try {
+    const existingUser = await UserModel.findOne({ username });
+    if (existingUser) {
+      res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await UserModel.create({
+      username: username,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ msg: "User Signup" });
+  } catch (err) {
+    res.status(500).json({ message: "Error creating user" });
+  }
+});
+
+app.post("/api/v1/signin", async (req, res) => {
+  const validatedData = userSchema.safeParse(req.body);
+  const { username, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      res.status(400).json({ message: "Invalid credentials" });
+    } else {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const token = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET as string
+      );
+      res.status(200).json({ token });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Login failed" });
+  }
+});
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {  
+  const link = req.body.link;
+  const type = req.body.type;
+  const title = req.body.title;
+  const tags = [];
+  //@ts-ignore
+  console.log(req.userId)
+  await ContentModel.create({
+    link:link,
+    type:type,
+    title: title,
+    tags : [],
+    //@ts-ignore
+    userId: req.userId
+  })
+
+  res.json({
+    msg:"content added"
+  })
+  
+});
+
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+  // @ts-ignore
+  const userId = req.userId;
+  const existingContent = await ContentModel.find({userId}).populate("userId", "username");
+
+  res.json({
+    content:existingContent
+  })
+
+});
+
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+  const contentId = req.body.contentId;
+
+  await ContentModel.deleteMany({
+    contentId,
+    // @ts-ignore
+    userId:req.userId
+  })
+
+  res.json({
+    msg: "Deleted"
+  })
+});
+
+app.post("/api/v1/brain/share", (req, res) => {
+  
+});
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
+
+app.listen(3000);
+
+console.log("listning");
---
+++ Backend/src/middleware.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+interface JwtPayload {
+  id: string;
+}
+
+// export interface userRequest extends Request {
+//   userId?: string;
+// }
+
+export const userMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeaders = req.headers["authorization"];
+    if (authHeaders) {
+      const decoded = jwt.verify(
+        authHeaders as string,
+        process.env.JWT_SECRET as string
+      ) as JwtPayload;
+      if (decoded) {
+        // @ts-ignore
+        req.userId = decoded.userId;
+        console.log("from middleware ",decoded)        
+        next();
+      } else {
+        res.status(403).json({ msg: "You are not logged In.." });
+      }
+    }
+  } catch (error) {
+    console.error("middleware error: ", error);
+  }
+};
