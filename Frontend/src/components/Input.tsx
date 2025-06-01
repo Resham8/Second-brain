864831623a0131af7d38b1300fa89ee284f231eb
Resham <reshamupadhyay48@gmail.com>
2025-06-01 22:19:34 +0530

    Update Input.tsx

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -1,11 +1,17 @@
-export default function Input({ onChange, placeholder }: { onChange: () => void, placeholder:string }) {
+export default function Input({
+  placeholder,
+  ref,
+}: {
+  placeholder: string;
+  ref?: any;
+}) {
   return (
     <div>
       <input
+        ref={ref}
         type={"text"}
         placeholder={placeholder}
         className="px-4 py-2 border rounded m-2"
-        onChange={onChange}
       />
     </div>
   );
