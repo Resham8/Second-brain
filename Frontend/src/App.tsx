b94d1986bcc018078b254eb072b816e2e5c1a6ce
Resham <reshamupadhyay48@gmail.com>
2025-06-01 22:19:46 +0530

    added react router dom

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Auth isSignup={true} />} />
+        <Route path="/signin" element={<Auth isSignup={false}/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
