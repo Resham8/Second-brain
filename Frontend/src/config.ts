5563894a79de63a67e03b99c4c0159f482b1f986
Resham <reshamupadhyay48@gmail.com>
2025-06-04 10:48:47 +0530

    Update config.ts

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,1 +1,13 @@
 export const BACKEND_URL = "http://localhost:3000";
+
+export enum ContentType {
+  Youtube = "youtube",
+  Twitter = "twitter",
+}
+
+export type BrainContent = {
+  _id: string;
+  title: string;
+  link: string;
+  type: ContentType;
+};
