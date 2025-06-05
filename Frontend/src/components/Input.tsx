7ce4254359a07b427e5d3999f966426513bf2a6b
Resham <reshamupadhyay48@gmail.com>
2025-06-05 21:25:31 +0530

    updated with react form

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -1,19 +1,24 @@
-export default function Input({
-  placeholder,
-  ref,
-}: {
+import type { RegisterOptions, UseFormRegister } from "react-hook-form";
+
+interface inputProps {
+  register: UseFormRegister<any>;
+  name: string;
   placeholder: string;
-  ref?: any;
-}) {
+  validations?:RegisterOptions;
+  error?: string;
+}
+
+export default function Input({ register, placeholder, name, validations, error }: inputProps) {
   return (
     <div>
       <input
-        ref={ref}
+        {...register(name,validations)}
+        id={name}
         type={"text"}
         placeholder={placeholder}
-        className="px-4 py-2 border rounded m-2"
+        className="px-4 py-2 border-gray-500 border rounded m-2"
       />
+      {error && <p className="text-red-500 text-sm mt-1 m-1">{error}</p>}
     </div>
   );
 }
- 
