ed1cb33e0588761f604a8df58f07351f321675dd
Resham <reshamupadhyay48@gmail.com>
2025-06-13 22:47:18 +0530

    added the search input

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -6,11 +6,12 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { getBrainContent, getShareLink } from "../api/api";
 import { useAuth } from "../state/useAuthStore";
 import { useFilterStore } from "../state/useFilterStore";
+import SearchInput from "../components/SearchInput";
 
 function Dashboard() {
   const logout = useAuth((state) => state.logout);
   const { activeType } = useFilterStore();
-
+  const searchItem = useFilterStore((state) => state.searchItem.toLowerCase());
   const { data: contents, isLoading } = useQuery({
     queryKey: ["content"],
     queryFn: () => getBrainContent(),
@@ -28,10 +29,13 @@ function Dashboard() {
     },
   });
 
-  const filteredContents =
-    activeType === "All Content"
-      ? contents
-      : contents?.filter((item) => item.type === activeType);
+  const filteredContents = (contents ?? [])
+    .filter((item) => activeType === "All Content" || item.type === activeType)
+    .filter(
+      (item) =>
+        item.title.toLowerCase().includes(searchItem) ||
+        item.tags?.some((tag) => tag.title.toLowerCase().includes(searchItem))
+    );
 
   if (isLoading) {
     <div>
@@ -59,20 +63,29 @@ function Dashboard() {
     <div>
       <Sidebar isShare={false} />
       <div className="p-5 ml-72 min-h-screen bg-gray-200">
-        <div className="flex justify-end gap-4">
-          <Button
-            variant="secondary"
-            text={isPending ? "Sharing..." : "Share Brain"}
-            startIcon={<Share2 size={18} />}
-            onClick={() => mutate()}
-          />
-          <Button
-            variant="primary"
-            text="Logout"
-            startIcon={<LogOut size={18} />}
-            onClick={logout}
-          />
+        <div className="p-3">
+          <h1 className="font-semibold text-3xl capitalize">{activeType}</h1>
         </div>
+
+        <div className="flex items-center justify-between mb-4">
+          <SearchInput />
+
+          <div className="flex items-center gap-3">
+            <Button
+              variant="secondary"
+              text={isPending ? "Sharing..." : "Share Brain"}
+              startIcon={<Share2 size={18} />}
+              onClick={() => mutate()}
+            />
+            <Button
+              variant="primary"
+              text="Logout"
+              startIcon={<LogOut size={18} />}
+              onClick={logout}
+            />
+          </div>
+        </div>
+
         <div className="columns-3 gap-5 pt-5">
           {filteredContents?.map(({ type, link, title, _id, tags }) => (
             <div key={_id} className="break-inside-avoid">
--- Frontend/src/state/useFilterStore.ts
+++ Frontend/src/state/useFilterStore.ts
@@ -6,9 +6,13 @@ type FilterType = ContentType | "All Content";
 interface FilterStore {
     activeType: FilterType;
     setActiveType: (type:FilterType) => void;
+    searchItem:string;
+    setSearchItem:(term:string) => void;
 }
 
 export const useFilterStore = create<FilterStore>((set) => ({
     activeType: "All Content",
-    setActiveType: (type) => set({activeType: type})
+    setActiveType: (type) => set({activeType: type}),
+    searchItem: "",
+    setSearchItem: (term) => set({ searchItem: term })
 }))
